f897761f0d2973b09a3e699674ecdcbce1331191
wiktor <wiktor.janiszewski10@gmail.com>
2024-07-16 23:21:51 +0200

    feat: save anime

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
+use std::fs;
+use std::path::Path;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
-use serde::Deserialize;
-
+use serde::{Deserialize, Serialize};
+use serde_json::*;
 async fn index() -> impl Responder {
     "Hello world!"
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct NewAnime {
     id: u32,
     title: String,
+    description: String,
 }
 #[post("/post")]
 async fn post(anime: web::Json<NewAnime>) -> Result<String> {
-    Ok(format!("Anime submitted: {}",anime.title))
+    create_file(NewAnime {
+        id: anime.id,
+        title: anime.title.clone(),
+        description: anime.description.clone(),
+    }).await;
+    Ok(format!("Anime submitted: {} \nid: {}", anime.title, anime.id))
 }
-
+async fn create_file(anime: NewAnime) {
+    
+    if !Path::new("./anime").exists() {
+        let _ = fs::create_dir("./anime");   
+    }
+    let _ = fs::write(format!("./anime/{}.json", anime.id), serde_json::to_string(&anime).unwrap());
+}
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
